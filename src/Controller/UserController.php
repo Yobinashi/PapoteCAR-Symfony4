3c1c8e9deb4704c48526e7d8fc92e76e1eff2f6e
Laetitia A <allard.laetitia@gmail.com>
2018-04-25 12:01:47 +0200

    retour à la normale *2

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Member;
 use App\Form\RegisterType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -11,36 +9,25 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
-
 class UserController extends Controller
 {
     /**
      * @Route("/register", name="register")
      */
     public function register(Request $req, EntityManagerInterface $em, UserPasswordEncoderInterface $enc){
-
         $member = new Member();
         $registerForm = $this->createForm(RegisterType::class, $member);
         $registerForm->handleRequest($req);
         $encoded= $enc->encodePassword($member, $member->getPassword());
         $member->setPassword($encoded);
-
         $member->setRoles(["ROLE_USER"]);
-
         if($registerForm->isSubmitted() && $registerForm->isValid()){
-
             $em->persist($member);
             $em->flush();
-
             return $this->redirectToRoute("home");
         }
-
         return $this->render('user/register.html.twig', ["registerForm"=>$registerForm->createView()]);
-
     }
-
-
     /**
      * @Route("/login", name="login")
      */
@@ -48,34 +35,26 @@ class UserController extends Controller
         if($this->getUser()){
             return $this->redirectToRoute('account');
         }
-
         $error = $auth->getLastAuthenticationError();
         $lastUsername = $auth->getLastUsername();
-
         return $this->render('user/login.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error
         ]);
-
+        var_dump('COUCOUCOUCOCUCOUC');
     }
-
     /**
      * @Route("/logout", name="logout")
      */
     public function logout(){
-
     }
-
-
     /**
      * @Route("/account", name="account")
      */
     public function account(){
-
         if(!$this->getUser()){
             $this->redirectToRoute('login');
         }
         return $this->render('user/account.html.twig');
-
     }
 }
