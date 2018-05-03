c47cb261183b1e0f661855c3f8d9f7fc0fa371ed
Laetitia A <allard.laetitia@gmail.com>
2018-05-03 16:35:12 +0200

    add file upload on registration

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -28,7 +28,7 @@ class RegisterType extends AbstractType
                 'second_options' => ['label' => 'Répéter le Mot de passe']
             ])
             ->add('tel')
-//            ->add('picture', FileType::class)
+            ->add('picture', FileType::class)
             ->add('vehicle')
             ->add('submit', SubmitType::class, ["label"=>"Créer mon profil"])
         ;
