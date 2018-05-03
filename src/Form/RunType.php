e8f2a11a7b53653a683a9835833170a823df3c78
unknown <Administrateur@N-PCP-033.IMIE.LAN>
2018-05-03 14:15:05 +0200

    Integration modification

--- src/Form/RunType.php
+++ src/Form/RunType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,7 +19,12 @@ class RunType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            //new DateTime permet de mettre une valeur par defaut égale à l'actuelle sur le formulaire
+            //departure et arrival sont des objets de type City. EntityType demande a symfo d'aller chercher en bdd
+                //toutes les entrées de City et les display en 'select'. Choice_label permet de choisir ce que va
+                //afficher le select a l'utilisateur, ici, le cityName
+            ->add('departure', TextType::class)
+            ->add('arrival', TextType::class)
+        //new DateTime permet de mettre une valeur par defaut égale à l'actuelle sur le formulaire
             ->add('departureSchedule', DateTimeType::class, ['data'=> new \DateTime()])
             //créé un select ['affichage' => 'valeur']
             ->add('places', ChoiceType::class, ['choices'=>[
@@ -26,16 +32,9 @@ class RunType extends AbstractType
                 '2'=> 2,
                 '3'=> 3,
                 '4'=> 4
-                                                                            ]])
+            ]])
             ->add('price', NumberType::class)
-            //departure et arrival sont des objets de type City. EntityType demande a symfo d'aller chercher en bdd
-                //toutes les entrées de City et les display en 'select'. Choice_label permet de choisir ce que va
-                //afficher le select a l'utilisateur, ici, le cityName
-            ->add('departure', EntityType::class,['class'=> City::class,
-                'choice_label'=> 'cityName'])
-            ->add('arrival',EntityType::class,['class'=> City::class,
-                'choice_label'=> 'cityName'])
-            ->add('submit', SubmitType::class, ["label"=>"Propose your run"])
+            ->add('submit', SubmitType::class, ["label"=>"Proposer le trajet"])
         ;
     }
 
